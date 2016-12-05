d7cf05d326af0779ee5c104c13699ceb2ce28d9f
Alessio Casini <kasoale@gmail.com>
2016-12-05 14:26:08 +0100

    added activity for filmdetails

--- app/src/main/java/show/ff/kasoale/it/ffshow/actities/FilmDetailsActivity.java
+++ app/src/main/java/show/ff/kasoale/it/ffshow/actities/FilmDetailsActivity.java
@@ -2,6 +2,7 @@ package show.ff.kasoale.it.ffshow.actities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -23,6 +24,7 @@ public class FilmDetailsActivity extends AppCompatActivity {
     private String IMG_URL_BASE = "https://image.tmdb.org/t/p/w500";
 
     private ImageView posterImage;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,10 @@ public class FilmDetailsActivity extends AppCompatActivity {
         Film film = (Film) getIntent().getSerializableExtra("Film");
         FilmDetail filmDetail = (FilmDetail) getIntent().getSerializableExtra("FilmDetail");
 
+        if(filmDetail == null){
+            //TODO: open popu no info available
+        }
+
         logger.info("*******");
         logger.info(filmDetail.toString());
         logger.info("*******");
@@ -40,6 +46,9 @@ public class FilmDetailsActivity extends AppCompatActivity {
         posterImage = (ImageView) findViewById(R.id.poster_image);
 
         Picasso.with(this.getApplicationContext()).load(imagePosterFull).into(posterImage);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(filmDetail.getTitle());
 
 
 
